9af9538fd7c3e6a8e4ff6ea15ea2e3e90c739c30
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-08-07 00:06:22 +0000

    feat: iniciando projeto

---
+++ src/app/api/chat/route.js
@@ -0,0 +1,5 @@
+import {openai} from "@ai-sdk/openai"
+
+export async function POST(request) {
+    const { messages } = await request.json();
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import styles from './header.module.css'
+
+import logo from './logo.png'
+import SearchInput from '../SearchInput'
+import { IconBell, IconCamera } from '../Icons'
+
+import avatar from './avatar.png'
+
+
+export const Header = () => {
+    return (<header className={styles.header}>
+        <div>
+            Vidy
+        </div>
+
+        <form className={styles.form}>
+            <SearchInput />
+        </form>
+
+        <ul className={styles.actions}>
+            <li>
+                <IconBell />
+            </li>
+            <li>
+                <IconCamera />
+            </li>
+            <li>
+                <Image src={avatar} className='rounded-full max-w-15'  alt='' />
+            </li>
+        </ul>
+    </header>)
+}
