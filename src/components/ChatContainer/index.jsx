c2ebb4c0f37a6745b32eb17ab26ae4f869ff2d01
Filipe Vilas Boas <fvb.boas@gmail.com>
2025-08-09 08:06:17 -0300

    feat: added scroll after message sent

--- src/components/ChatContainer/index.jsx
+++ src/components/ChatContainer/index.jsx
@@ -9,60 +9,72 @@ import { Loader } from "../Loader";
 import { RetryButton } from "../RetryButton";
 import styles from "./container.module.css";
 import { useChat } from "@ai-sdk/react";
-import { DefaultChatTransport } from 'ai';
-import { useEffect, useState } from 'react'
+import { DefaultChatTransport } from "ai";
+import { useRef, useState } from "react";
 
 export const ChatContainer = () => {
-  const [input, setInput] = useState('')
-  const { messages, sendMessage, status, stop, setMessages, regenerate, error } = useChat({
+  const [input, setInput] = useState("");
+  const ref = useRef(null);
+
+  const {
+    messages,
+    sendMessage,
+    status,
+    stop,
+    setMessages,
+    regenerate,
+    error,
+  } = useChat({
     transport: new DefaultChatTransport({
-      api: '/api/chat',
+      api: "/api/chat",
     }),
-    // messages: [{ parts: [{ type: 'text', text: 'hello' }] }]
+    // messages: [{ parts: [{ type: "text", text: "hello" }] }],
   });
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await sendMessage({ text: input });
     } catch (error) {
       console.error("Erro ao enviar mensagem:", error);
     }
-    setInput('');
+    setInput("");
+    ref.current.scrollBy(0, 100);
   };
   const handleRemove = (msgId) => {
-    setMessages(messages.filter(msg =>msg.id !== msgId))
-  }
+    setMessages(messages.filter((msg) => msg.id !== msgId));
+  };
 
   return (
     <section className={styles.container}>
-      <div className={`${styles.chat} mt-10`}>
+      <div ref={ref} className={`${styles.chat} mt-10 border-2`}>
         {messages.map((msg) => (
           <ChatBubble
             onRemove={() => handleRemove(msg.id)}
-            isUser={msg.role === 'user'}
+            isUser={msg.role === "user"}
             key={msg.id}
-            message={msg.parts.find(p => p.type === 'text').text}
+            message={msg.parts.find((p) => p.type === "text").text}
           />
         ))}
       </div>
-  
-      {
-        status == 'streaming' &&
+
+      {status == "streaming" && (
         <div>
           <Loader />
           <Button variant="danger" onClick={stop}>
             <IconStop /> Parar
           </Button>
         </div>
-      }
-      {
-        (status !== 'streaming' && messages.length > 0) && <><RetryButton onClick={regenerate} /></>
-      }
+      )}
+      {status !== "streaming" && messages.length > 0 && (
+        <>
+          <RetryButton onClick={regenerate} />
+        </>
+      )}
       {error && <p>Ops, alguma coisa deu errado!</p>}
       <ChatForm
-        disabled={status == 'streaming'}
+        disabled={status == "streaming"}
         input={input}
-        handleInputChange={e => setInput(e.target.value)}
+        handleInputChange={(e) => setInput(e.target.value)}
         handleSubmit={handleSubmit}
       />
     </section>
